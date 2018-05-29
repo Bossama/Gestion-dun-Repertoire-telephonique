71295f7ce7956921d024aee9feedba236dcaa7e2
Bossama <32778390+Bossama@users.noreply.github.com>
2018-05-29 12:04:43 +0100

    Update Sauvgardee.c

--- Sauvgardee.c
+++ Sauvgardee.c
@@ -11,12 +11,12 @@ FILE *fichier = NULL;
 printf("...Sauvgarde TERMINER. \n");
 fichier = fopen("contactes.txt", "a+");
 
-    fseek(fichier, 0, SEEK_END); // place le curseur à la fin du fichier
+    fseek(fichier, 0, SEEK_END); // place le curseur Ã  la fin du fichier
 
     for (i=1;i<=nbr_contact;i++){
          if (fichier != NULL)
-    {// On peut lire et écrire dans le fichie
-     // On l'écrit dans le fichier
+    {// On peut lire et Ã©crire dans le fichie
+     // On l'Ã©crit dans le fichier
 
          fprintf(fichier,"%s\t\t %s\t\t %d\t\t %s\t\t\n",tab[i].nom,tab[i].prenom,tab[i].num,tab[i].ncin);
 
@@ -27,66 +27,5 @@ fichier = fopen("contactes.txt", "a+");
     }
     fclose(fichier);
 
-
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//#include <stdio.h>
-//#include "prototypes.h"
-//#include "Structeurs.h"
-//
-//
-//void sauvegarder(contact tab[],int nbr_contact)
-//{
-//    FILE *fichier;
-//    int i;
-//
-//    fichier=fopen("contactes.txt","a+");
-//    fseek(fichier,0,SEEK_END);
-//
-//    for(i=0;i<nbr_contact;i++)
-//    {
-//         fprintf(fichier,"%s\t\t %s\t\t %d\t\t %s\t\t\n",tab[i].nom,tab[i].prenom,tab[i].num,tab[i].ncin);
-//    }
-//    fclose(fichier);
-//}
